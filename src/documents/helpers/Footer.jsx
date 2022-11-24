e802bb10d8eec3a689e060f81f6320848a3a08ca
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-11-24 15:53:02 -0600

    En el footer se agregan las redes sociales ig y tw

--- src/documents/helpers/Footer.jsx
+++ src/documents/helpers/Footer.jsx
@@ -1,7 +1,14 @@
 const Footer = () => {
     return(
         <footer id="footer">
+            <a href="">
+            <img id="ig" src="../../src/logotipo-de-instagram.png" alt="" />
+            </a>
             <p>© 2022-2022 | Doña Bertha & Don Fernando</p>
+            
+            <a href="">
+            <img id="tw" src="../../src/logotipo-de-twitter.png" alt="" />
+            </a>
         </footer>
     )
 }
